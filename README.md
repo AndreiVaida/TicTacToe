5834ff406c9b65f00a3fdb9d685768605eaa9d96
Andrei Vaida <andrei_vd2006@yahoo.com>
2025-09-27 13:37:07 +0300

    Setup app for deployment
    The app is now shareable across all devices (computer/smartphone).
    Instructions to avoid blocking by CORS policy:
    1. Run `npm run build`! Two files will be generated in `/dist` folder: `index.html` and `bundle.js`.
    2. Open `index.html`! Delete `<script>` from `<head>`!
    3. Add `<script> </script>` inside `<body>`, right after (below) `<div id="root"></div>`!
    4. Open `bundle.js` and copy its content!
    5. Paste the `bundle.js` content inside the new `<script> </script>`!
    6. Save `index.html`! Delete `bundle.js`!
    7. Copy-Paste `index.html` on any device! Open an play!

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Game developed by Andrei Lucian Vaida 🎮
 
 This is a React app (with TypeScript) created with Vite.
+
+## Deployment
+TicTacToe is playable on browser (computer and smartphone).
+
+Instructions:
+1. Run `npm run build`! Two files will be generated in `/dist` folder: `index.html` and `bundle.js`.
+1. Open `index.html`! Delete `<script>` from `<head>`!
+1. Add `<script> </script>` inside `<body>`, right after (below) `<div id="root"></div>`!
+1. Open `bundle.js` and copy its content!
+1. Paste the `bundle.js` content inside the new `<script> </script>`!
+1. Save `index.html`! Delete `bundle.js`!
+1. Copy-Paste `index.html` on any device! Open an play!
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,22 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    build: {
+        target: 'es2015',
+        assetsInlineLimit: 100000000,
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+                entryFileNames: 'bundle.js',
+                format: 'iife'
+            },
+        },
+    },
 });
